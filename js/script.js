cd2bc68c29c5388c0b9af85ace4530aa795b53c9
sakeyehp <sakeyehp@yahoo.co.uk>
2019-11-18 07:37:29 +0000

    add changes to admin dashboard

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
-$("#exampleModal").modal()
+$("#exampleModal").modal();
 
-var ctx = document.getElementById('myChart');
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// function updateUrl(url,key,value){
+//     if(value!==undefined){
+//         value = encodeURI(value);
+//     }
+//     var hashIndex = url.indexOf("#")|0;
+//     if (hashIndex === -1) hashIndex = url.length|0;
+//     var urls = url.substring(0, hashIndex).split('?');
+//     var baseUrl = urls[0];
+//     var parameters = '';
+//     var outPara = {};
+//     if(urls.length>1){
+//         parameters = urls[1];
+//     }
+//     if(parameters!==''){
+//         parameters = parameters.split('&');
+//         for(k in parameters){
+//             var keyVal = parameters[k];
+//             keyVal = keyVal.split('=');
+//             var ekey = keyVal[0];
+//             var evalue = '';
+//             if(keyVal.length>1){
+//                 evalue = keyVal[1];
+//             }
+//             outPara[ekey] = evalue;
+//         }
+//     }
+//
+//     if(value!==undefined){
+//         outPara[key] = value;
+//     }else{
+//         delete outPara[key];
+//     }
+//     parameters = [];
+//     for(var k in outPara){
+//         // parameters.push(k + '=' + outPara[k]);
+//         parameters.push(k + '' + outPara[k]);
+//     }
+//
+//     var finalUrl = baseUrl;
+//
+//     if(parameters.length>0){
+//         finalUrl += parameters.join('&');
+//         // finalUrl += '?' + parameters.join('&');
+//     }
+//
+//     return finalUrl + url.substring(hashIndex);
+// }
